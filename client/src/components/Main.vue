164aedd1e61262168a470b42206505a0fa0d2da2
DESKTOP-AI3KLJL\Alex Colba <pashavarava1806@gmail.com>
2024-07-28 20:11:49 +0300

    Provided basic example to send request from client to API

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -1,32 +1,50 @@
 <template>
     <v-container fluid>
-
-        <v-carousel show-arrows="hover" cycle hide-delimiter-background>
-            <v-carousel-item
-                src="https://images.unsplash.com/photo-1721213126988-3450888e603a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                cover></v-carousel-item>
-            <v-carousel-item
-                src="https://images.unsplash.com/photo-1721148774311-5634b1de8cfc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                cover></v-carousel-item>
-
-            <v-carousel-item
-                src="https://images.unsplash.com/photo-1721213375079-1a626470ecfe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                cover></v-carousel-item>
-        </v-carousel>
+      <v-carousel show-arrows="hover" cycle hide-delimiter-background>
+        <v-carousel-item
+          src="https://images.unsplash.com/photo-1721213126988-3450888e603a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          cover
+        ></v-carousel-item>
+        <v-carousel-item
+          src="https://images.unsplash.com/photo-1721148774311-5634b1de8cfc?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          cover
+        ></v-carousel-item>
+  
+        <v-carousel-item
+          src="https://images.unsplash.com/photo-1721213375079-1a626470ecfe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          cover
+        ></v-carousel-item>
+      </v-carousel>
     </v-container>
-</template>
-
-<script>
-
-    export default {
-        name: 'MainComponent',
-        components: {
-        },
-        data() {
-            return {
-            }
-        },
-    };
-</script>
-
-<style></style>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    name: "MainComponent",
+    components: {},
+    data() {
+      return {
+        categories: [],
+      };
+    },
+    mounted() {
+      this.fetchCategories();
+    },
+    methods: {
+      async fetchCategories() {
+        try {
+          const response = await axios.get("https://localhost:44329/api/Category/getAll");
+          this.categories = response.data;
+          console.log(this.categories); // Перевірка отриманих даних
+        } catch (error) {
+          console.error("Error fetching categories:", error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style></style>
+  
