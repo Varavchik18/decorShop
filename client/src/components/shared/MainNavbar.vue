09f82db3696eb7064e1111220e0dc3881a783fc6
DESKTOP-AI3KLJL\Alex Colba <pashavarava1806@gmail.com>
2024-07-29 14:09:30 +0300

    Added prototype of navbar

--- client/src/components/shared/MainNavbar.vue
+++ client/src/components/shared/MainNavbar.vue
@@ -1,77 +1,69 @@
 <template>
-    <div>
-        <v-app-bar app color="primary" dark>
-            <v-toolbar-title>
-                <router-link to="/" class="text-decoration-none white--text">
-                    <v-img src="@/assets/logo.png" max-height="40" max-width="40" contain class="ml-5" />
-                </router-link>
-            </v-toolbar-title>
+    <v-toolbar app color="primary" :elevation="5">
+        <v-toolbar-title>
+            <router-link to="/" class="text-decoration-none white--text">
+                <v-img src="@/assets/logo.png" max-height="40" max-width="40" contain class="ml-5" />
+            </router-link>
+        </v-toolbar-title>
 
-            <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-            <v-text-field v-model="search" append-inner-icon="mdi-magnify" label="Пошук" variant="underlined"
-                single-line hide-details style="max-width: 300px;" base-color="black" clearable
-                class="mr-5"></v-text-field>
+        <v-text-field v-model="search" append-inner-icon="mdi-magnify" label="Пошук" variant="underlined" single-line
+            hide-details style="max-width: 300px;" base-color="black" clearable class="mr-5"></v-text-field>
 
-            <v-btn icon @click="$router.push('/favorites')" class="mt-3">
-                <v-icon>mdi-heart</v-icon>
-            </v-btn>
-            <v-btn icon @click="$router.push('/cart')" class="mt-3">
-                <v-icon>mdi-cart</v-icon>
-            </v-btn>
+        <v-btn icon @click="$router.push('/favorites')" class="mt-3">
+            <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn icon @click="$router.push('/cart')" class="mt-3">
+            <v-icon>mdi-cart</v-icon>
+        </v-btn>
 
-            <template v-slot:extension>
-                <v-menu open-on-hover>
+        <template v-slot:extension>
+            <v-tabs v-model="currentItem" grow hide-slider>
+                <v-tab v-for="item in items" :key="item" :text="item"></v-tab>
+
+                <v-menu v-if="more.length">
                     <template v-slot:activator="{ props }">
-                        <v-tabs v-model="tab" grow hide-slider>
-                            <v-tab v-bind="props">CATEGORY 1</v-tab>
-                            <v-tab v-bind="props">CATEGORY 2</v-tab>
-                            <v-tab v-bind="props">CATEGORY 3</v-tab>
-                        </v-tabs>
+                        <v-btn class="align-self-center me-4" variant="plain" v-bind="props">
+                            more
+
+                            <v-icon icon="mdi-menu-down" end></v-icon>
+                        </v-btn>
                     </template>
 
-                    <v-list>
-                        <v-list-item v-if="tab === 0">
-                            <v-list-item-title> SUBCATEGORY 1 for FIRST TAB</v-list-item-title>
-                            <v-list-item-title> SUBCATEGORY 2 for FIRST TAB</v-list-item-title>
-                            <v-list-item-title> SUBCATEGORY 3 for FIRST TAB</v-list-item-title>
-                        </v-list-item>
-                        <v-list-item v-if="tab === 1">
-                            <v-list-item-title> SUBCATEGORY 1 for SECOND TAB</v-list-item-title>
-                            <v-list-item-title> SUBCATEGORY 2 for SECOND TAB</v-list-item-title>
-                            <v-list-item-title> SUBCATEGORY 3 for SECOND TAB</v-list-item-title>
-                        </v-list-item>
-                        <v-list-item v-if="tab === 2">
-                            <v-list-item-title> SUBCATEGORY 1 for THIRD TAB</v-list-item-title>
-                            <v-list-item-title> SUBCATEGORY 2 for THIRD TAB</v-list-item-title>
-                            <v-list-item-title> SUBCATEGORY 3 for THIRD TAB</v-list-item-title>
-                        </v-list-item>
+                    <v-list class="bg-grey-lighten-3">
+                        <v-list-item v-for="item in more" :key="item" :title="item"
+                            @click="addItem(item)"></v-list-item>
                     </v-list>
                 </v-menu>
+            </v-tabs>
+        </template>
+    </v-toolbar>
 
-
-            </template>
-        </v-app-bar>
-    </div>
 </template>
 
 <script>
     export default {
-        name: 'MainNavbar',
-        data() {
-            return {
-                tab: null,
-                listItems: [
-                    { title: 'Click Me' },
-                    { title: 'Click Me' },
-                    { title: 'Click Me' },
-                    { title: 'Click Me 2' },
-                ],
-                items: [
-                    'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
-                ],
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            }
+        data: () => ({
+            currentItem: 'tab-Web',
+            items: [
+                'Web', 'Shopping', 'Videos', 'Images',
+            ],
+            more: [
+                'News', 'Maps', 'Books', 'Flights', 'Apps',
+            ],
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        }),
+
+        methods: {
+            addItem(item) {
+                const removed = this.items.splice(0, 1)
+                this.items.push(
+                    ...this.more.splice(this.more.indexOf(item), 1),
+                )
+                this.more.push(...removed)
+                this.$nextTick(() => { this.currentItem = 'tab-' + item })
+            },
         },
     }
 </script>
@@ -81,13 +73,11 @@
         font-size: 14px;
     }
 
-    .panel {
-        position: absolute;
-        top: 100%;
-        left: 0;
-        right: 0;
-        background-color: white;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        z-index: 100;
+    .v-tabs {
+        flex-grow: 1;
+    }
+
+    .v-container {
+        padding-top: 20px;
     }
 </style>
